806e3d1d29d1bf5d6a17db69318b4a5bbac22a59
code-magician <priyanshkumar001@gmail.com>
2023-12-30 17:19:14 +0530

    Styled Skills Section

--- src/container/Skills/SubContainers/SkillCard.jsx
+++ src/container/Skills/SubContainers/SkillCard.jsx
@@ -4,21 +4,32 @@ import { urlFor } from '../../../client';
 
 
 function SkillCard(props) {
+    const dragVariant = {
+        dragElastic: 1,
+        dragConstraints: {
+            top: -25,
+            left: -25,
+            right: 25,
+            bottom: 25,
+        },
+        dragSnapToOrigin: true
+    }
+
     return (
         <motion.div
-            whileInView={{ opacity: [0, 1] }}
-            transition={{ duration: 0.5 }}
+            whileInView={{ scale: 1, opacity: 1 }}
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale:0.9, opacity: 0.75 }}
             className="app__skills-item app__flex"
+
+            drag
+                dragElastic={dragVariant.dragElastic}
+                dragConstraints={dragVariant.dragConstraints}
+                dragSnapToOrigin={dragVariant.dragSnapToOrigin}
         >
-            <motion.div
-                whileInView={{ scale: 1, opacity: 1 }}
-                whileHover={{ opacity: 1.5 }}
-                whileTap={{ opacity: 0.75 }}
-                className="app__flex"
-                style={{ backgroundColor: props.item.bgColor }}
-            >
-                <img src={urlFor(props.item.icon)} alt={props.item.name} />
-            </motion.div>
+            <div className="app__flex">
+                <img draggable={false} src={urlFor(props.item.icon)} alt={props.item.name} />
+            </div>
 
             <p className="p-text">{props.item.name}</p>
         </motion.div>
