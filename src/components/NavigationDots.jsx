8ae38d072b9589f65fb41db33b8e4fc0cd50c81b
code-magician <priyanshkumar001@gmail.com>
2023-12-30 12:12:49 +0530

    Changed active and non active navigation dots animation

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -7,18 +7,35 @@ const NavigationDots = ({ active }) => {
 
             {['home', 'about', 'work', 'skills', 'certificates', 'contact'].map(
                 (item, index) => (
-                    <>
-                        <motion.div
+                    active === item ?
+                        (<motion.div
                             key={item + index}
-
-                            whileInView={{ scale: 1.5 }}
-                            whileHover={{ scale: 2.5 }}
+                            animate={{
+                                scale: [1.5, 2.5, 2.5, 1.5, 1.5],
+                                rotate: [0, 0, 180, 180, 0],
+                                borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+                            }}
+                            transition={{
+                                duration: 2,
+                                ease: "easeInOut",
+                                times: [0, 0.2, 0.5, 0.8, 1],
+                                repeat: Infinity,
+                                repeatDelay: 1
+                            }}
 
                             onClick={() => { window.open(`#${item}`, '_self') }}
                             className="app__navigation-dot"
-                            style={active === item ? { backgroundColor: '#313BAC' } : {}}
-                        />
-                    </>
+                            style={{ backgroundColor: '#313BAC' }}
+                        />) :
+                        (<motion.div
+                            key={item + index}
+                            whileInView={{ scale: 1.2 }}
+                            whileHover={{ scale: 2 }}
+                            transition={{ ease: "easeOut", duration: 0.3 }}
+
+                            onClick={() => { window.open(`#${item}`, '_self') }}
+                            className="app__navigation-dot"
+                        />)
                 )
             )}
         </div>
