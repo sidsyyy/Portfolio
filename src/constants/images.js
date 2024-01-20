89b41c16de20ab5ee014c96853c6c73c2ed3857c
code-magician <priyanshkumar001@gmail.com>
2024-01-20 18:59:58 +0530

    Profile

--- src/constants/images.js
+++ src/constants/images.js
@@ -23,7 +23,7 @@ import about02 from "../assets/about02.png";
 import about03 from "../assets/about03.png";
 import about04 from "../assets/about04.png";
 
-import profile from "../assets/Profile.png";
+import profile from "../assets/ProfileSid.png";
 import circle from "../assets/circle.png";
 import logo from "../assets/P-logo.png";
 
